6b8a7b0b0538ceb4c6e845efa16e2feff0ae6361
Kyle Taylor <kyletaylored@gmail.com>
2016-02-21 23:03:25 -0600

    attempting an Instagram feed api

--- js/custom.js
+++ js/custom.js
@@ -23,6 +23,20 @@ $(document).ready(function(){
   if($('.bxslider').length){
     $('.bxslider').bxSlider();
   }
+
+  /*
+    ==============================================================
+       Instafeed.js
+    ============================================================== */
+  var feed = new Instafeed({
+        get: 'tagged',
+        tagName: 'opendata',
+        // userId: 'opendenton',
+        clientId: '2d3bd18548014b11b75fa8c2367551a3',
+        target: 'instagram-gallery',
+        template: '<li class="col-xs-4"><a href="{{link}}"><img src="{{image}}" /></a></li>'
+    });
+  feed.run();
     
   /*
   ==============================================================
